892774974a2950c1dfdedad745ea6d670b55d548
Umar4t7 <158988215+Umar4t7@users.noreply.github.com>
2024-02-23 07:50:57 +0500

    Update config.js

--- config.js
+++ config.js
@@ -9,10 +9,10 @@ global.antitags = true;
 
 
 //auto functioner
-global.autoTyping = false;                //auto tying by default off.
+global.autoTyping = true;                //auto tying by default off.
 global.autoRecord = false;                //auto recording by default off.
 global.groupevent = false;                //This is the new variable for controlling group event handling.
-global.statusseen = false;                 //This is the new variable for controlling status seen.
+global.statusseen = true;                 //This is the new variable for controlling status seen.
 global.autoreadgc = false;
 
 
@@ -21,15 +21,15 @@ global.prefa = ["."];                                    //Default prefix here. 
 
 
 //
-global.Owner = ["+94704274987", "+94704274987"];         //If you want singal number so global.Owner = ['916297175943'] Change into your number.
-global.OwnerNumber = ["+94704274987", "+94704274987"];   //If you want singal number so global.Owner = ['916297175943'] Change into your number.
-global.ownertag = ["+94704274987"];
-global.OwnerName = "SACHITH";
-global.BotName = "SACHITH";
-global.packname = "🦋 │𝐂𝐘𝐁𝜩𝐑│𝐏𝜟𝐍𝐃𝐀│𝐌𝐃│𝐕➂ 🦋";                             //Do not change.
-global.author = "SACHITH";                               //Do not change.
+global.Owner = ["+12495231178", "+12495231178"];         //If you want singal number so global.Owner = ['12495231178'] Change into your number.
+global.OwnerNumber = ["+12495231178", "+12495231178"];   //If you want singal number so global.Owner = ['12495231178'] Change into your number.
+global.ownertag = ["+12495231178"];
+global.OwnerName = "≼╙≽≼𝗨𝗠𝗔𝗥≽≼╜≽";
+global.BotName = "≼╙≽≼𝗨𝗠𝗔𝗥≽≼╜≽";
+global.packname = "🦋 BLOCKBUSTER | KING | UMAR  🦋";                             //Do not change.
+global.author = "≼╙≽≼𝗨𝗠𝗔𝗥≽≼╜≽";                               //Do not change.
 global.BotSourceCode = "https://github.com/Kai00"; //Do not change.
-global.SupportGroupLink = "https://chat.whatsapp.com/GhRGdwfaMVDCoeAdzILfl";
+global.SupportGroupLink = "https://chat.whatsapp.com/Hwo2RhSbkW91pPzxw4HTgB";
 
 
 //
@@ -43,17 +43,17 @@ global.openAiAPI = "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";
 //
 global.location = "HambanThota, Sri Lankan";
 global.reactmoji = "💞";
-global.themeemoji = "😂";
+global.themeemoji = "🙂";
 global.vidmenu = { url: 'https://tenor.com/en-CA/view/anonymous-hacker-smooth-gif-10171911193797826570%9D%90%95-12-14' };
 global.websitex = "https://github.com/CYBER-x-SACHIYA-SL-MD-BOT/CYBER-PANDA-MD.V.0.4";
-global.lolhuman = "Sachith";
+global.lolhuman = "≼╙≽≼𝗨𝗠𝗔𝗥≽≼╜≽";
 
 
 //
-global.BotLogo = fs.readFileSync("./Assets/pic1.jpg");
-global.Thumb = fs.readFileSync("./Assets/pic9.jpg");
-global.Thumb1 = fs.readFileSync("./Assets/pic5.jpg");
-global.ErrorPic = fs.readFileSync("./Assets/pic7.jpg");
+global.BotLogo = fs.readFileSync("https://telegra.ph/file/6e9111a989f493b974fcb.jpg");
+global.Thumb = fs.readFileSync("https://telegra.ph/file/6e9111a989f493b974fcb.jpg");
+global.Thumb1 = fs.readFileSync("https://telegra.ph/file/6e9111a989f493b974fcb.jpg");
+global.ErrorPic = fs.readFileSync("https://telegra.ph/file/6e9111a989f493b974fcb.jpg");
 
 
 //
@@ -80,18 +80,18 @@ global.ntilink = []
 //
 global.mess = {
   jobdone: 'Here you go...',
-  useradmin: 'Sorry, only *Group Admins* can use this command *Baka*!',
+  useradmin: 'Sorry, only *Group Admins* can use this command *≼╙≽≼𝗨𝗠𝗔𝗥≽≼╜≽*',
   botadmin: 'Sorry, i cant execute this command without being an *Admin* of this group.',
-  botowner: 'Only my *Owner* can use this command, Baka!',
-  grouponly: 'This command is only made for *Groups*, Baka!',
-  privateonly: 'This command is only made for *Private Chat*, Baka!',
+  botowner: 'Only my *≼╙≽≼𝗨𝗠𝗔𝗥≽≼╜≽* can use this command, ≼╙≽≼𝗨𝗠𝗔𝗥≽≼╜≽,
+  grouponly: 'This command is only made for *Groups*, ≼╙≽≼𝗨𝗠𝗔𝗥≽≼╜≽',
+  privateonly: 'This command is only made for *Private Chat*, ≼╙≽≼𝗨𝗠𝗔𝗥≽≼╜≽',
   botonly: 'Only the *Bot itself* can use this command!',
   waiting: 'Just Wait...',
-  nolink: 'Please provide me *link*, Baka!',
-  error: 'An error occurd!',
-  banned: 'You are *Banned* fron using commands!',
-  bangc: 'This Group is *Banned* from using Commands!',
-  nonsfw: 'Dont be a pervert Baka! This is not a NSFW enabled group!'
+  nolink: 'Please provide me *link*, ≼╙≽≼𝗨𝗠𝗔𝗥≽≼╜≽,
+  error: 'An error occurd! ≼╙≽≼𝗨𝗠𝗔𝗥≽≼╜≽',
+  banned: '≼╙≽≼𝗨𝗠𝗔𝗥≽≼╜≽*Banned* to you fron using commands!',
+  bangc: '≼╙≽≼𝗨𝗠𝗔𝗥≽≼╜≽ *Banned* this group from using Commands!',
+  nonsfw: 'Dont be a pervert This is not a NSFW enabled group!'
 
 }
 
